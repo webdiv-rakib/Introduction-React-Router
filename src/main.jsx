d1ff1972a6d63efbf8d8355f090550c3b0cac8e8
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-03-08 19:03:33 +0600

    What is SPA, routing and react router setup

--- src/main.jsx
+++ src/main.jsx
@@ -2,38 +2,28 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+
+// For React Router
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
-import Root from './components/Root/Root.jsx';
-import Home from './components/Home/Home.jsx'
-import Mobiles from './components/Mobiles/Mobiles.jsx'
-import Laptops from './components/Laptops/Laptops.jsx';
 const router = createBrowserRouter([
   {
     path: "/",
-    Component: Root,
-    children: [
-      { index: true, Component: Home },
-      { path: 'mobiles', Component: Mobiles },
-      { path: 'laptops', Component: Laptops }
-    ]
+    element: <div>Hello From React Router!!!</div>,
   },
   {
-    path: "about",
-    element: <div>About Me Here</div>
+    path: "/about",
+    element: <div>I am in About Page</div>
   },
   {
-    path: "contact",
-    element: <div>Contact Us Here</div>
-  },
-  {
-    path: 'app',
-    Component: App
+    path: "/contact",
+    element: <div>Call me right now</div>
   }
 ]);
 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router={router}></RouterProvider>
   </StrictMode>,
 )
