f0d350a64ebe718eb7819c4c2274b09ae3c7f824
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-03-11 17:25:58 +0600

    Active route, Loading Spinner, useLocation, useNavigation

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,10 @@
-import { Outlet, useNavigation } from "react-router";
+import { Outlet, useLocation, useNavigation } from "react-router";
 import Header from '../Header/Header'
 import Footer from "../Footer/Footer";
 const Home = () => {
     const navigation = useNavigation();
+    const location = useLocation();
+    console.log(location);
     return (
         <div>
             <Header></Header>
